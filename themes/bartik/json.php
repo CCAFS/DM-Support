c04f610d3ebe3ff15ed393418caf7eaa22dcf4bc
sebas932 <sebas932@gmail.com>
2014-06-17 14:22:54 -0500

    Added GPL V3 License

--- themes/bartik/json.php
+++ themes/bartik/json.php
@@ -1,4 +1,27 @@
 <?php 
+/**
+*  This file is part of Data Management Support Pack.
+*
+*  DMSP is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  at your option) any later version.
+*
+*  DMSP is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with DMSP.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Copyright 2014 (C) Climate Change, Agriculture and Food Security (CCAFS)
+* 
+* Created on : 20-12-2013
+* @author      Sebastian Amariles Garcia (CIAT/CCAFS)
+* @version     1.0
+*/
+
 /** bootstrap Drupal **/ 
 chdir("../../");
 define('DRUPAL_ROOT', "./");
